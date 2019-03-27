acdf9f8d1f4b69ecd84bb2ffa5547c9d2059b48d
jkpawlowski <jkpawlowski96@gmail.com>
2019-03-27 23:49:17 +0100

    stock viewer

---
+++ Lab3/Lab03 Forex/StockApp/StockApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+       private  Task LoadStock(string symbol)
+        {
+            return Task.Factory.StartNew(async () => {
+                string data = await StockConnection.LoadDataAsync(symbol);
+                Stock stock = StockReader.Read(data);
+                await Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    CompanyBox.Text = stock.CompanyName;
+                    SymbolBox.Text = stock.Symbol;
+                    PriceBox.Text = stock.priceToSales.ToString();
+                }));
+
+            });
+        }
+        private async void LoadPrice_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await LoadStock(SymbolSelectBox.Text);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+    }
+}
---
+++ Lab3/Lab03 Forex/StockApp/StockApp/StockConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp
+{
+    class StockConnection
+    {
+        
+        static string apiKey = "1b6714e500f0cdd864a8b49ec6ac5e45";
+        static string apiBaseUrl = "https://api.iextrading.com/1.0/stock/";
+
+        public static async Task<string> LoadDataAsync(string symbol)
+        {
+            string apiCall = apiBaseUrl + symbol + "/stats";
+            Task<string> result;
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(apiCall))
+            using (HttpContent content = response.Content)
+            {
+                result = content.ReadAsStringAsync();
+            }
+            return await result;
+        }
+    }
+}
+
